bc64bd8c4a2e8ae39c7ba9aec1efe7577eced30f
olllegborisov <olllegborisov@gmail.com>
2024-01-14 00:07:05 +0300

    18-controlled-input

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <>
+      <h1>Oleg</h1>
+      <h3>I am Watching React course</h3>
+      <button>Button</button>
+    </>
+  )
+}
+
+export default Card
---
+++ 18-controlled-input/src/components/login.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    const userData = {
+      username,
+      password,
+    }
+    console.log(userData)
+    alert(JSON.stringify(userData))
+  }
+
+  return (
+    <>
+      <h1>Login Form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  )
+}
+
+export default Login
