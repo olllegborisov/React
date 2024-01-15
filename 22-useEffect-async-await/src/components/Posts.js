2aedc6a65ec6feb1a379cc6af469d27bab8992a2
olllegborisov <olllegborisov@gmail.com>
2024-01-15 22:34:57 +0300

    22-useEffect-async-await

--- 22-useEffect-async-await/src/components/Posts.js
+++ 22-useEffect-async-await/src/components/Posts.js
@@ -6,15 +6,29 @@ const API_URL = 'https://jsonplaceholder.typicode.com/posts'
 function Posts() {
   const [posts, setPosts] = useState([])
   const [error, setError] = useState('')
-  const [isLoading, setIsloading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((posts) => setPosts(posts))
-      .catch((error) => setError(error.message))
-      .finally(() => setIsloading(false))
+    async function fetchData() {
+      try {
+        const res = await fetch(API_URL)
+        const posts = await res.json()
+        setPosts(posts)
+      } catch (error) {
+        setError(error.message)
+      }
+      setIsLoading(false)
+    }
+    fetchData()
   }, [])
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((res) => res.json())
+  //     .then((posts) => setPosts(posts))
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsloading(false))
+  // }, [])
 
   if (error) {
     return <h1>Error: {error}</h1>
