ac89bb34ef7c22a935c1dd1a5fdf568e3b44fc74
5420 <olllegborisov@gmail.com>
2024-02-14 18:35:53 +0300

    Added BookForm, BookLIst, and Filter components

--- 29-pure-redux/index.js
+++ 29-pure-redux/index.js
@@ -1,17 +1,18 @@
 import store from './redux/store.js'
-import getCurrentTime from './utils/getCurrentTime.js'
+import { addCurrentTime, clearTimes } from './redux/actionCreators.js'
 
 const addTimeBtin = document.getElementById('addTime')
-addTimeBtin.addEventListener('click', () =>
-    store.dispatch({
-        type: 'ADD_CURRENT_TIME',
-        payload: getCurrentTime()
-    })
+addTimeBtin.addEventListener('click', () => store.dispatch(addCurrentTime())
+)
+
+const clearTimesBtn = document.getElementById('clearTimes')
+clearTimesBtn.addEventListener('click', () => store.dispatch(clearTimes())
 )
 
 const timesList = document.getElementById('timesList')
 
 store.subscribe(() => {
+    timesList.innerHTML = ''
     const times = store.getState()
     times.forEach((time) => {
         let li = document.createElement('li')
