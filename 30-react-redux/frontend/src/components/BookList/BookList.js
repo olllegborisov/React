976b86a2238e51e950a98320662422d1f8a20255
olllegborisov <olllegborisov@gmail.com>
2024-06-10 23:38:46 +0300

    hightlight filter text matter

--- 30-react-redux/frontend/src/components/BookList/BookList.js
+++ 30-react-redux/frontend/src/components/BookList/BookList.js
@@ -11,7 +11,7 @@ const BookList = () => {
     const dispatch = useDispatch()
     const books = useSelector((state) => state.books)
     const titleFilter = useSelector(selectTitleFilter)
-    const titleAuthor = useSelector(selectAuthorFilter)
+    const authorFilter = useSelector(selectAuthorFilter)
     const onlyFavoriteFilter = useSelector(selectOnlyFavoriteFilter)
     // books.forEach(element => {
     //     console.log(element.id);
@@ -29,7 +29,7 @@ const BookList = () => {
 
     const filteredBooks = books.filter((book) => {
         const matchesTitle = book.title.toLowerCase().includes(titleFilter.toLowerCase())
-        const matchesAuthor = book.author.toLowerCase().includes(titleAuthor.toLowerCase())
+        const matchesAuthor = book.author.toLowerCase().includes(authorFilter.toLowerCase())
         // if (onlyFavoriteFilter && book.isFavorite === !onlyFavoriteFilter) {
         //     return false
         // }
@@ -38,7 +38,21 @@ const BookList = () => {
     })
 
 
+    const hightLightMAtch = (text, filter) => {
+        if (!filter) return text
 
+        const regex = new RegExp(`(${filter})`, 'gi')
+        return text.split(regex).map((substring, i) => {
+            if (substring.toLowerCase() === filter.toLowerCase()) {
+                return (
+                    <span key={i} className='highlight'>
+                        {substring}
+                    </span>
+                )
+            }
+            return substring
+        })
+    }
 
 
 
@@ -52,7 +66,7 @@ const BookList = () => {
                 <ul>
                     {filteredBooks.map((book, i) => (
                         <li key={book.id}>
-                            <div className="book-info">{++i}. {book.title} by <strong>{book.author}</strong><div className="book-actions">
+                            <div className="book-info">{++i}. {hightLightMAtch(book.title, titleFilter)} by <strong>{hightLightMAtch(book.author, authorFilter)}</strong><div className="book-actions">
                                 <span onClick={() => handleToggleFavorite(book.id)}>
                                     {book.isFavorite ? (
                                         <BsBookmarkStarFill className="star-icon" />
