1a42c2e9ed4089a2b35c426a4da2d8834968cc9d
olllegborisov <olllegborisov@gmail.com>
2024-06-17 22:05:59 +0300

    1232

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,44 +1,43 @@
-import { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { addBook, fetchBook } from '../../redux/slices/booksSlice'
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaSpinner } from 'react-icons/fa';
+import {
+    addBook,
+    fetchBook,
+    selectIsLoadingViaAPI,
+} from '../../redux/slices/booksSlice';
 import { setError } from '../../redux/slices/errorSlice';
-import createBookWithID from '../../utils/createBookWithID'
-import booksData from '../../data/books.json'
-import './BookForm.css'
+import createBookWithID from '../../utils/createBookWithID';
+import booksData from '../../data/books.json';
+import './BookForm.css';
 
 const BookForm = () => {
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    const dispatch = useDispatch()
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const isLoadingViaAPI = useSelector(selectIsLoadingViaAPI);
+    const dispatch = useDispatch();
 
     const handleAddRandomBook = () => {
-        const randomIndex = Math.floor(Math.random() * booksData.length)
-        const randomBook = booksData[randomIndex]
-        dispatch(addBook(createBookWithID(randomBook, 'random')))
-    }
-    // consst [formData, setFormData] = useState({})
+        const randomIndex = Math.floor(Math.random() * booksData.length);
+        const randomBook = booksData[randomIndex];
+        dispatch(addBook(createBookWithID(randomBook, 'random')));
+    };
+
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
 
         if (title && author) {
-            // const book = {
-            //     title: title,
-            //     author: author,
-            //     id: uuidv4()
-            // }
-            dispatch(addBook(createBookWithID({ title, author }, 'manual')))
-            setTitle('')
-            setAuthor('')
+            dispatch(addBook(createBookWithID({ title, author }, 'manual')));
+            setTitle('');
+            setAuthor('');
         } else {
             dispatch(setError('You must fill title and author!'));
         }
-    }
-
-
+    };
 
     const handleAddRandomBookViaAPI = () => {
-        dispatch(fetchBook())
-    }
+        dispatch(fetchBook('http://localhost:4000/random-book-delayed'));
+    };
 
     return (
         <div className="app-block book-form">
@@ -46,18 +45,44 @@ const BookForm = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="title">Title: </label>
-                    <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="author">Author: </label>
-                    <input type="text" id="author" value={author} onChange={(e) => setAuthor(e.target.value)}></input>
+                    <input
+                        type="text"
+                        id="author"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                    />
                 </div>
                 <button type="submit">Add Book</button>
-                <button type="button" onClick={handleAddRandomBook}>Add Random</button>
-                <button type="button" onClick={handleAddRandomBookViaAPI}> Add Random via API</button>
+                <button type="button" onClick={handleAddRandomBook}>
+                    Add Random
+                </button>
+
+                <button
+                    type="button"
+                    onClick={handleAddRandomBookViaAPI}
+                    disabled={isLoadingViaAPI}
+                >
+                    {isLoadingViaAPI ? (
+                        <>
+                            <span>Loading Book...</span>
+                            <FaSpinner className="spinner" />
+                        </>
+                    ) : (
+                        'Add Random via API'
+                    )}
+                </button>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default BookForm
+export default BookForm;
