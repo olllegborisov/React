83508d246eacddc0534451e5641003b7aac65763
5420 <olllegborisov@gmail.com>
2024-08-01 17:24:05 +0300

    contact form was added

--- service-center/frontend/src/components/contacts/parent-component/Contacts.jsx
+++ service-center/frontend/src/components/contacts/parent-component/Contacts.jsx
@@ -1,9 +1,11 @@
 import ContactsInfo from "../ContactsInfo"
+import ContactsForm from "../ContactsForm"
 
 const Contacts = () => {
   return (
     <>
         <ContactsInfo />
+        <ContactsForm />
     </>
   )
 }
--- service-center/frontend/src/ui-components/Input.jsx
+++ service-center/frontend/src/ui-components/Input.jsx
@@ -1,9 +1,9 @@
 import styles from '../ui-components/Input.module.css'
 
-const Input = ({register, errors, required, placeholder }) => {
+const Input = ({register, errors, required, placeholder, className }) => {
     const inputName = register.name
   return (
-    <div className={styles.inputWrapper}>
+    <div className={`${styles.inputWrapper} ${className}`}>
         <input className={styles.input}  placeholder={placeholder} {...register}/>
         <p className={styles.error}>{errors[inputName]?.message}</p>
     </div>
